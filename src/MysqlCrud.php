121fe3eb8086bf990c739e42f70143e49d02790b
Ruslan Akhmetov <slan.ru@gmail.com>
2017-04-01 00:50:14 +0300

    New

---
+++ src/MysqlCrud.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: win10
+ * Date: 01.04.2017
+ * Time: 0:22
+ */
+
+namespace DB;
+
+
+class MysqlCrud
+{
+    public function test(){
+
+        return 1;
+    }
+
+}
